70069cd90a5d656581cc263e19f84cc2075db95a
George <baczxh@gmail.com>
2017-04-03 14:34:14 +0930

    commit the branch feat_gst1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -33,8 +33,9 @@ namespace ProductApps
             {
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
-                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
-                totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment+25);
+                totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment+5);
+                totalchargeAfterGSTtb.Text = Convert.ToString((cProduct.TotalPayment+25+5)*1.1m);
             }
             catch (FormatException)
             {
